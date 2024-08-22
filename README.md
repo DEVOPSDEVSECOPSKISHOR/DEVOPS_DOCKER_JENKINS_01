9faa1798af88199c258a3e00108f5830a43bec5a
kishor <kishorab023@gmail.com>
2024-08-22 09:25:53 +0530

    created a maven server - ssh server and modified the jenkins container

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 docker-compose up --build -d
 
 
+for Docker ssh agent
+
+cd agents
+
+docker build -t ssh_server_image .
+
+docker run -d -p 2222:22 --name ssh_server_container ssh_server_image
--- powersellStartDocker.ps1
+++ powersellStartDocker.ps1
@@ -1,4 +1,4 @@
-docker-compose up -d jenkins-blueocean
+docker-compose up -d 
 $process = Start-Process -FilePath "docker-compose" -ArgumentList "build" -Wait -PassThru
 Write-Host "Build process finished with exit code: $($process.ExitCode)"
 
@@ -9,5 +9,7 @@ if ($process.ExitCode -eq 0) {
 }
 
 Write-Host "Attempting to retrieve initial admin password..."
+
+Start-Sleep -Seconds 10
 
 docker exec devops_arch_docker_01-jenkins-1 cat /var/jenkins_home/secrets/initialAdminPassword
